b77a0e1e3cc299abb767c1ae2e06d94713e52d25
Victor Lelis <ovictorlelis@gmail.com>
2024-12-08 08:04:39 -0300

    finalizado

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -3,9 +3,32 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class Home extends Component
 {
+    public $cnpj = '';
+    public $dados;
+    public $erro;
+    public $carregando = false;
+
+    public function consultarCnpj()
+    {
+        $this->carregando = true;
+        $this->erro = null;
+        $this->dados = null;
+
+        $response = Http::get("https://minhareceita.org/{$this->cnpj}");
+
+        if ($response->successful()) {
+            $this->dados = $response->json();
+        } else {
+            $this->erro = 'CNPJ não encontrado ou erro na consulta.';
+        }
+
+        $this->carregando = false;
+    }
+
     public function render()
     {
         return view('livewire.home');
